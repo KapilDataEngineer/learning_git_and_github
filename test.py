0bd3972c9229525cb596fb75be2d561ec07445af
KapilDataEngineer <kapilc.614@gmail.com>
2025-10-18 17:57:57 +0530

    3rd commit done in github directly

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ a = input("enter the name")
 print(f"hey,{a} how are you")
 b = input("your villagae name ")
 print(f"{b} this is your village name")
+
+c = input("hey lets make some changes 1")
+print(f"{c})
